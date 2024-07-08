3939b3f54ccdf5225d5cd818c949e68453e3bc05
Alan Gabriel <contato@alangabriel.dev>
2024-07-07 22:46:54 -0300

    editor layout

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,6 +3,11 @@
 import { fabric } from "fabric";
 import { useEffect, useRef } from "react";
 
+import { Footer } from "@/features/editor/components/footer";
+import { Navbar } from "@/features/editor/components/navbar";
+import { Sidebar } from "@/features/editor/components/sidebar";
+import { Toolbar } from "@/features/editor/components/toolbar";
+
 import { useEditor } from "@/features/editor/hooks/use-editor";
 
 export const Editor = () => {
@@ -25,8 +30,20 @@ export const Editor = () => {
 
   return (
     <div className="flex flex-col h-full">
-      <div className="flex-1 h-full bg-muted" ref={containerRef}>
-        <canvas ref={canvaRef} />
+      <Navbar />
+
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar />
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+          <div
+            className="flex-1 h-[calc(100%-124px)] bg-muted"
+            ref={containerRef}
+          >
+            <canvas ref={canvaRef} />
+          </div>
+          <Footer />
+        </main>
       </div>
     </div>
   );
---
+++ src/features/editor/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+  return (
+    <aside className="w-[100px] bg-white flex flex-col h-full border-r overflow-y-auto">
+      {/* aside */}
+    </aside>
+  );
+};
